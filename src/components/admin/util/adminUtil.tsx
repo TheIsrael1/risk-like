e47258c85268b36014170a9331b9de3bba147da3
TheIsrael1 <iehindero419@stu.ui.edu.ng>
2022-10-11 17:22:25 +0100

    :fire: phase 2 mvp

--- src/components/admin/util/adminUtil.tsx
+++ src/components/admin/util/adminUtil.tsx
@@ -1,41 +1,24 @@
-
-
-    export const objectParser = (obj: any) =>{
-        if(obj instanceof Array){   
-            const body = obj?.map((o: any, idx: number)=>{
-                return (
-                    <div key={idx}  className="propertyWrapper">
-                            {objectParser(o)}
-                    </div>
-                )
-            })
-            return body?.length  ?
-            (
-                <div className="value">
-                    {body}
-                </div>
-            )  : `[ ]`
-        }else if(typeof obj === "object"){
-            const body =  Object.entries(obj)?.map(([key, value], idx)=>(
-                <div key={idx} className="property">
-                    <div className="key">
-                            {key}
-                    </div>
-                     {objectParser(value) }
-                </div>
-            ))
-            return body.length ? body : "{ }"
-        }else if(obj === undefined){
-             return(
-                <div className="value">
-                    No Data
-                </div>  
-             )        
-        }else{
-            return (
-                <div className="value">
-                    {`${obj}`.length ? `${obj}` : "---"}
-                </div>
-            )
-    }
-}
+export const objectParser = (obj: any) => {
+  if (obj instanceof Array) {
+    const body = obj?.map((o: any, idx: number) => {
+      return (
+        <div key={idx} className="propertyWrapper">
+          {objectParser(o)}
+        </div>
+      );
+    });
+    return body?.length ? <div className="value">{body}</div> : `[ ]`;
+  } else if (typeof obj === "object" && obj !== null) {
+    const body = Object.entries?.(obj)?.map(([key, value], idx) => (
+      <div key={idx} className="property">
+        <div className="key">{key}</div>
+        {objectParser(value)}
+      </div>
+    ));
+    return body.length ? body : "{ }";
+  } else if (obj === undefined) {
+    return <div className="value">No Data</div>;
+  } else {
+    return <div className="value">{`${obj}`.length ? `${obj}` : "---"}</div>;
+  }
+};
